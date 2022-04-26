82c6edde239bd95cb58761d5d08671c8b860ce2e
Danish <iamdanishk555@gmail.com>
2022-04-26 16:56:48 +0545

    Prepared the AddHappyPlaceActivity UI using dimens, string, color

--- app/src/main/java/com/example/happyplacesapp/AddHappyPlaceActivity.kt
+++ app/src/main/java/com/example/happyplacesapp/AddHappyPlaceActivity.kt
@@ -2,10 +2,25 @@ package com.example.happyplacesapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.happyplacesapp.databinding.ActivityAddHappyPlaceBinding
 
 class AddHappyPlaceActivity : AppCompatActivity() {
+
+    private var binding: ActivityAddHappyPlaceBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_happy_place)
+        binding = ActivityAddHappyPlaceBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        setSupportActionBar(binding?.toolbarAddPlace)
+        if (supportActionBar != null){
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+            supportActionBar?.title = "ADD HAPPY PLACE"
+        }
+
+        binding?.toolbarAddPlace?.setNavigationOnClickListener {
+            onBackPressed()
+        }
     }
 }
